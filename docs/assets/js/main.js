3274eb8fe24602d5ab77cd310421035e39efb659
Bargiel <bargiel@zon-29382.localdomain>
2018-12-21 18:02:23 +0100

    add masonry

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,19 +1,21 @@
 function ready(fn) {
 	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
 		fn();
+		console.log('ready!');
 	} else {
 		document.addEventListener('DOMContentLoaded', fn);
 	}
 }
 
 function masonry() {
+	console.log('masonry!');
 	var grids = document.querySelectorAll('.grid');
 
 	for(var i = 0; i < grids.length; i++) {
 		new Masonry( grids[i], {
   		// options
   		itemSelector: '.grid-item',
-  		columnWidth: 216
+  		columnWidth: 286
   	});
 	}
 }
