070f5c5398e983894b897d5f841cb7cf209f6865
Bargiel <bargiel@zon-29382.localdomain>
2018-12-21 18:37:40 +0100

    add individual post html

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,19 +1,3 @@
-// $(document).ready(function(){
-// 	var grids = document.querySelectorAll('.grid');
-
-// 	for(var i = 0; i < grids.length; i++) {
-// 		grids[i] = $(grids[i]).masonry({
-// 			// options
-//   			itemSelector: '.grid-item',
-//   			columnWidth: 286
-// 		});
-
-// 		grids[i].imagesLoaded().progress( function() {
-//   			grids[i].masonry('layout');
-// 		});
-// 	}
-// });
-
 $(document).ready(function(){
 	var $grid = $('.grid').masonry({
 		itemSelector: '.grid-item',
@@ -23,5 +7,3 @@ $(document).ready(function(){
 		$grid.masonry('layout');
 	});
 });
-
-// layout Masonry after each image loads
